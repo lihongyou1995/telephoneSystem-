7fb19e151d6b8cae0c5596a121379029dc4a3cdd
lihongyou <1311571542@qq.com>
2020-07-29 00:53:38 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ export default {
     return{
       lastTime: null, //最后一次点击的时间
       currentTime: null, //当前点击的时间
-      timeOut: 60 * 60 * 1000 //设置超时时间
+      timeOut: 60 * 60 * 1000, //设置超时时间
     }
   },
   methods: {
@@ -19,20 +19,20 @@ export default {
       if(sessionStorage.getItem('userID')){
           this.currentTime = new Date().getTime(); //记录这次点击的时间
           this.lastTime = sessionStorage.getItem('lastTime')
-          if(this.currentTime - this.lastTime > this.timeOut) { 
+          if(this.currentTime - this.lastTime > this.timeOut) {
                 this.$message('由于您长时间未操作，登录已过期，请重新登录')
                 sessionStorage.clear();
                   this.$router.push({
                       path:'/'
                 })
           } else {
-              sessionStorage.setItem("lastTime",new Date().getTime()); 
+              sessionStorage.setItem("lastTime",new Date().getTime());
           }
         }
       }
     },
     mounted(){
-      
+
     },
     mounted(){
       this.currentTime = new Date().getTime(); //记录这次点击的时间
